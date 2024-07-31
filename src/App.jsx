41657d049a22b75b2a099910286866c54bae86c0
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2024-07-31 12:45:51 +0300

    base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/UI/Header"
+import Hero from "./components/UI/Hero"
+import hero_background from './assets/hero-background.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="h-full w-full bg-cover bg-center bg-no-repeat relative" style={{backgroundImage: `url(${hero_background})`}}>
+      <Header />
+      <Hero />
+    </div>
   )
 }
 
